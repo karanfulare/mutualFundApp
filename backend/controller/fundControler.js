dbec12c69a782939096084c142d9231c7d8704d9
KaranFulare <fularekaran@yahoo.com>
2025-03-01 11:27:20 +0530

    user authentication done

--- backend/controller/fundControler.js
+++ backend/controller/fundControler.js
@@ -6,7 +6,7 @@ const query = util.promisify(db.query).bind(db);
 module.exports = {
 fetchFund: async(req,res)=>{
     try {
-        return res.status(200)
+        return res.status(200).json({message:"working validated"})
     } catch (err) {
         console.error(err)
     }
--- backend/routes/fundRoutes.js
+++ backend/routes/fundRoutes.js
@@ -1,3 +1,8 @@
 const router = require("express").Router() ;
 
+const {fetchFund} = require("./../controller/fundControler");
+const{ verifyToken} = require("../middleware/auth")
+
+router.get("/list-funds",verifyToken,fetchFund)
+
 module.exports = router ;
